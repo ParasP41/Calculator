6a901e7ccb7024a3cf99c36e0e1a8539b1ca11a4
ParasP41 <parasvishwakarma198@gmail.com>
2024-08-30 22:59:57 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+let inp=  document.querySelector('input')
+
+let p=document.getElementById("pi");
+p.addEventListener("click", function()
+{
+    let v=Math.PI;
+    p.textContent=v;
+      
+});
+
+ let btn=  document.querySelectorAll('button')
+   for(let i of btn){
+    i.addEventListener('click',(e)=>{
+      let text=e.target.innerText
+
+        if(text==='C'){
+            inp.value=''
+        }
+        else if(text==='='){
+            inp.value=eval(inp.value)
+        }
+        else{
+            inp.value=inp.value+text
+        }
+        
+    })
+
+   }
